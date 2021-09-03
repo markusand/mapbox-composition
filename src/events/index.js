9bd2234a46db9582f4d602fc51cb45c560acbe06
mvilella <mvilella@obsa.ad>
2021-09-03 15:56:54 +0200

    feat: Add map event callbacks on map init options

--- src/events/index.js
+++ src/events/index.js
@@ -1,6 +1,7 @@
+import useMapEvents from './events.map';
 import useSourceEvents from './events.source';
 import useLayerEvents from './events.layer';
 import useMarkerEvents from './events.marker';
 import usePopupEvents from './events.popup';
 
-export { useSourceEvents, useLayerEvents, useMarkerEvents, usePopupEvents };
+export { useMapEvents, useSourceEvents, useLayerEvents, useMarkerEvents, usePopupEvents };
--- src/map.js
+++ src/map.js
@@ -1,8 +1,10 @@
 import { Map } from 'mapbox-gl';
+import { useMapEvents } from './events';
 import { debounce } from './utils';
 
 export default async function useMap(container, options = {}) {
 	const map = new Map({ container, ...options });
+	useMapEvents(map, options);
 	await map.once('load');
 
 	const resize = new ResizeObserver(debounce(() => map.resize()));
