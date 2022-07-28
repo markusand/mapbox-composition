f2ce839082be5ab374567fb5ca4efa473c4739f0
Marc Vilella <mvilella.and@gmail.com>
2022-07-28 12:41:03 +0200

    feat: bbox helpers works on any geometry type

--- src/helpers.ts
+++ src/helpers.ts
@@ -1,10 +1,25 @@
 import { LngLatBounds } from 'mapbox-gl';
-import { toArray } from './utils';
+import type { LngLatLike } from 'mapbox-gl';
+import type { FeatureCollection, Feature, Geometry, Position } from 'geojson';
 
-const bbox = geojson => {
-	const features = geojson.features || toArray(geojson);
+type GeometryType = Exclude<Geometry['type'], 'GeometryCollection'>
+
+const bbox = (geojson: FeatureCollection | Feature) => {
+	const depth: Record<GeometryType, number> = {
+		Point: 0,
+		MultiPoint: 1,
+		LineString: 1,
+		MultiLineString: 2,
+		Polygon: 2,
+		MultiPolygon: 3,
+	};
+
+	const features = 'features' in geojson ? geojson.features : [geojson];
 	return features.reduce((acc, { geometry }) => {
-		geometry.coordinates.forEach(coords => acc.extend(coords));
+		if (geometry.type !== 'GeometryCollection') {
+			const coordinates = [geometry.coordinates].flat(depth[geometry.type]) as Position[];
+			coordinates.forEach(coordinate => acc.extend(coordinate as LngLatLike));
+		}
 		return acc;
 	}, new LngLatBounds());
 };
