146e50107f38875fba6824cb5cc610504500238d
Marc Vilella <mvilella.and@gmail.com>
2022-07-29 18:21:14 +0200

    chore: Build process on ts instead rollup

--- rollup.config.js
+++ rollup.config.js
@@ -8,24 +8,24 @@ import { terser } from 'rollup-plugin-terser';
 import packageJson from './package.json';
 
 export default {
-	input: 'src/index.js',
-	output: [
-		{
-			format: 'cjs',
-			file: packageJson.main,
-			sourcemap: true,
-		},
-		{
-			format: 'esm',
-			file: packageJson.module,
-			sourcemap: true,
-		},
-	],
-	plugins: [
-		peerDepsExternal(),
-		resolve(),
-		commonjs(),
-		terser(),
-		postcss({ plugins: [autoprefixer()] }),
-	],
+  input: 'src/index.js',
+  output: [
+    {
+      format: 'cjs',
+      file: packageJson.main,
+      sourcemap: true,
+    },
+    {
+      format: 'esm',
+      file: packageJson.module,
+      sourcemap: true,
+    },
+  ],
+  plugins: [
+    peerDepsExternal(),
+    resolve(),
+    commonjs(),
+    terser(),
+    postcss({ plugins: [autoprefixer()] }),
+  ],
 };
