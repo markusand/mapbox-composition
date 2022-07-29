2d4c15b597ecdf0dc15d88009476299f6d45d7f0
Marc Vilella <mvilella.and@gmail.com>
2022-07-29 15:57:52 +0200

    fix: Add Async methods export type

--- src/async.ts
+++ src/async.ts
@@ -1,4 +1,6 @@
-import type { Map } from 'mapbox-gl';
+import type { Map, MapboxEvent, EventData } from 'mapbox-gl';
+
+type Event = MapboxEvent<MouseEvent | TouchEvent | WheelEvent | undefined> & EventData;
 
 export const ACTIONS = ['panBy', 'panTo', 'zoomTo', 'zoomIn', 'zoomOut', 'rotateTo', 'resetNorth', 'snapToNorth', 'fitBounds', 'fitScreenCoordinates', 'jumpTo', 'easeTo', 'flyTo'] as const;
 
@@ -7,10 +9,10 @@ export default (map: Map, actions = ACTIONS) => actions.reduce((acc, action) => 
 	acc[action] = (...options: any[]) => new Promise(resolve => {
 		(map[action] as any)(...options);
 		map.fire(`${eventName}start`);
-		map.once('moveend', event => {
+		map.once('moveend', (event: Event) => {
 			map.fire(`${eventName}end`);
 			resolve(event);
 		});
 	});
 	return acc;
-}, {} as Record<typeof ACTIONS[number], any>);
+}, {} as Record<typeof ACTIONS[number], (...options: any[]) => Promise<Event>>);
