7139c6a196ea86e4a7fda2d2531c7073f828b63f
Marc Vilella <mvilella.and@gmail.com>
2022-08-04 11:59:29 +0200

    feat: Provide mapbox native options to image loader

--- src/images.ts
+++ src/images.ts
@@ -1,31 +1,39 @@
 import type { Map } from 'mapbox-gl';
 
+type ImageOptions = { pixelRatio?: number; sdf?: boolean };
+
 export default (map: Map) => {
   let persistImages: Record<string, string> = {};
   let persistListener: ((ev: unknown) => void);
 
-  const loadImage = ([name, path]: string[]): Promise<void> => new Promise((resolve, reject) => {
-    if (map.hasImage(name)) return resolve();
-    return map.loadImage(path, (error, image) => {
-      if (error) return reject(error);
-      if (image) map.addImage(name, image);
-      return resolve();
+  const loadImage = ([name, path]: string[], options?: ImageOptions): Promise<void> => {
+    return new Promise((resolve, reject) => {
+      if (map.hasImage(name)) return resolve();
+      return map.loadImage(path, (error, image) => {
+        if (error) return reject(error);
+        if (image) map.addImage(name, image, options);
+        return resolve();
+      });
     });
-  });
+  };
 
   const addImages = async (
     images: Record<string, string>,
-    options?: { persist?: boolean },
+    options?: {
+      persist?: boolean;
+      pixelRatio?: number;
+      sdf?: boolean;
+    },
   ): Promise<void> => {
-    const { persist = true } = options || {};
+    const { persist = true, ...rest } = options || {};
     if (persist) {
       persistImages = { ...persistImages, ...images };
       if (!persistListener) {
-        persistListener = () => Object.entries(persistImages).map(loadImage);
+        persistListener = () => Object.entries(persistImages).map(image => loadImage(image, rest));
         map.on('style.load', persistListener);
       }
     }
-    const loaded = Object.entries(images).map(loadImage);
+    const loaded = Object.entries(images).map(image => loadImage(image, rest));
     await Promise.all(loaded);
   };
 
