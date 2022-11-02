aa7404925a3c69940eec4d19cc71428b78eff1bd
Marc Vilella <mvilella.and@gmail.com>
2022-11-03 00:18:03 +0100

    feat: Enable setting a complete source object

--- src/geojson.ts
+++ src/geojson.ts
@@ -1,22 +1,29 @@
-import type { Map, GeoJSONSourceRaw, GeoJSONSource } from 'mapbox-gl';
+import type { Map, GeoJSONSourceRaw, GeoJSONSource, GeoJSONSourceOptions } from 'mapbox-gl';
 import type { Feature, Geometry, FeatureCollection } from 'geojson';
 import useLayer, { BaseLayerOptions } from './layer';
 
 export type GeoJSONLayerOptions = {
-  source: Feature<Geometry> | FeatureCollection<Geometry> | string,
+  source: GeoJSONSourceOptions | Feature<Geometry> | FeatureCollection<Geometry> | string,
 } & Omit<BaseLayerOptions, 'type' | 'source'>;
 
+const isString = (item: any): item is string => typeof item === 'string';
+const isGeo = (item: any): item is Feature<Geometry> | FeatureCollection<Geometry> => (
+  'type' in item && (item.type === 'Feature' || item.type === 'FeatureCollection')
+);
+
+const getSource = (source: any) => isString(source) || isGeo(source) ? { data: source } : source;
+
 export default (map: Map, options: GeoJSONLayerOptions) => {
-  // Adapt to GeoJSON source format
-  const geojson = { data: options.source };
+  const geojson = getSource(options.source);
+
   const layer = useLayer(map, {
     ...options,
     type: 'geojson',
-    source: geojson as GeoJSONSourceRaw,
+    source: geojson,
   });
 
   // Override. GeoJSON can be updated by setting new data
-  const updateSource = (data: GeoJSONLayerOptions['source']) => {
+  const updateSource = (data: Feature<Geometry> | FeatureCollection<Geometry> | string) => {
     const source = map.getSource(options.name) as GeoJSONSource;
     if (source) source.setData(data);
     else layer.updateSource({ data } as GeoJSONSourceRaw);
