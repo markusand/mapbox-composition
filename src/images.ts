3d29b409e5657ba795eabae2fa22ef8120b49fd0
Marc Vilella <mvilella.and@gmail.com>
2023-08-05 13:50:10 +0200

    refactor: Simplify images loading

--- src/images.ts
+++ src/images.ts
@@ -1,54 +1,49 @@
 import type { Map } from 'mapbox-gl';
 
-type ImageOptions = { pixelRatio?: number; sdf?: boolean };
+type Image = HTMLImageElement | ImageBitmap;
+
+type ImageOptions = {
+  persist?: boolean,
+  pixelRatio?: number;
+  sdf?: boolean,
+};
+
+const toArray = <T>(item: T | T[]) => Array.isArray(item) ? item : [item];
+
+const cache: Record<string, { image: Image, options?: ImageOptions }> = {};
 
 export default (map: Map) => {
-  let persistImages: Record<string, string> = {};
-  let persistListener: ((ev: unknown) => void) | undefined;
-
-  const loadImage = ([name, path]: string[], options?: ImageOptions): Promise<void> => {
-    return new Promise((resolve, reject) => {
-      if (map.hasImage(name)) return resolve();
-      return map.loadImage(path, (error, image) => {
-        if (error) return reject(error);
-        // Check image again. It may have been loaded meanwhile
-        if (image && !map.hasImage(name)) map.addImage(name, image, options);
-        return resolve();
-      });
+  const reloadCache = () => {
+    Object.entries(cache).forEach(([name, { image, options }]) => {
+      if (!map.hasImage(name)) map.addImage(name, image, options);
     });
   };
 
-  const addImages = async (
-    images: Record<string, string>,
-    options?: {
-      persist?: boolean;
-      pixelRatio?: number;
-      sdf?: boolean;
-    },
-  ): Promise<void> => {
-    const { persist = true, ...rest } = options || {};
-    if (persist) {
-      persistImages = { ...persistImages, ...images };
-      if (!persistListener) {
-        persistListener = () => Object.entries(persistImages).map(image => loadImage(image, rest));
-        map.on('style.load', persistListener);
-      }
-    }
-    const loaded = Object.entries(images).map(image => loadImage(image, rest));
-    await Promise.all(loaded);
+  const loadImage = (path: string) => new Promise<Image>((resolve, reject) => {
+    map.loadImage(path, (error, image) => {
+      if (error || !image) return reject(error);
+      resolve(image);
+    });
+  });
+
+  const addImages = async (paths: Record<string, string>, options?: ImageOptions) => {
+    const { persist = true } = options || {};
+    const loading = Object.entries(paths).map(async ([name, path]) => {
+      const image = await loadImage(path);
+      if (!map.hasImage(name)) map.addImage(name, image, options);
+      if (persist) cache[name] = { image, options };
+    });
+    await Promise.all(loading);
   };
 
-  const removeImages = (images: string | string[]): void => {
-    const names = Array.isArray(images) ? images : Object.keys(images);
-    names.forEach(name => {
+  const removeImages = (names: string | string[]) => {
+    toArray(names).forEach(name => {
       if (map.hasImage(name)) map.removeImage(name);
-      delete persistImages[name];
+      delete cache[name];
     });
-    if (!Object.keys(persistImages).length && persistListener) {
-      map.off('style.load', persistListener);
-      persistListener = undefined;
-    }
   };
+
+  map.on('style.load', reloadCache);
 
   return { addImages, removeImages };
 };
