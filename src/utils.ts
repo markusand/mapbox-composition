53d4b0ad42c05b672be9db46c819355ffd8a068b
Marc Vilella <mvilella.and@gmail.com>
2023-08-07 00:27:31 +0200

    refactor: Clear utilities

--- src/utils.ts
+++ src/utils.ts
@@ -6,12 +6,6 @@ export const uuid = (): string => Math.random().toString(36).substring(7);
 
 export const capitalize = (str: string): string => `${str.charAt(0).toUpperCase()}${str.slice(1)}`;
 
-export const isObject = (item: unknown): boolean => (
-  !!item && (item as { constructor: ObjectConstructor }).constructor === Object
-);
-
-export const toArray = <T>(item: T): T[] => (Array.isArray(item) ? item : [item]);
-
 export const debounce = <T extends any[]>(callback: (...args: T) => void, delay: number) => {
   let timeout: ReturnType<typeof setTimeout>;
   return (...args: T) => {
